782417abb1392df89a011d55e5fd9a19692e555c
MaxMillington <maxmillington89@gmail.com>
2015-09-10 17:03:39 -0600

    trying to style feed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def render_profile_picture(feed)
+    feed.caption.from.profile_picture if feed.caption
+  end
+
+  def render_username(feed)
+    feed.caption.from.username if feed.caption
+  end
+
+  def render_text(feed)
+    feed.caption.text if feed.caption
+  end
 end
