2af0537dde1cbf8d03cf833281e262ccf3a6bcc1
MaxMillington <maxmillington89@gmail.com>
2015-09-11 13:39:51 -0600

    added authorize to profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,7 @@
 class ProfileController < ApplicationController
 
+  before_action :authorize!
+
   def show
     @profile = current_user.client.user_recent_media(count: 100).
         paginate(page: params[:page], per_page: 9)
