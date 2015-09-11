658b510011d6bdc26169c92f77a6831308b04a06
MaxMillington <maxmillington89@gmail.com>
2015-09-11 13:38:17 -0600

    added authorize

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
 
+  before_action :authorize!
+
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get '/auth/instagram', as: :login
   get  'auth/instagram/callback', to: 'sessions#create'
   get  '/logout', to: 'sessions#destroy'
-  get  '/dashboard', to: 'dashboard#show'
   get 'dashboard/feed', to: 'dashboard#index'
   get 'dashboard/popular', to: 'dashboard#popular'
   get '/user-search', to: 'dashboard#user_search'
