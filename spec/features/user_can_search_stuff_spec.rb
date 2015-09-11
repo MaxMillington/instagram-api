0230f95ee6f17d740ba6c85851fc0a240cf1e907
MaxMillington <maxmillington89@gmail.com>
2015-09-11 10:58:19 -0600

    added search users test

--- spec/features/user_can_search_stuff_spec.rb
+++ spec/features/user_can_search_stuff_spec.rb
@@ -11,14 +11,40 @@ RSpec.describe "User can search stuff", type: :feature do
         click_link "Login"
 
         expect(current_path).to eq(dashboard_feed_path)
+        within("#tag-search") do
+          fill_in "search", with: ("monkey")
+          click_link_or_button("search-tags")
+          expect(current_path).to eq(search_path)
+        end
 
-        fill_in "search", with: ("monkey")
-        click_link_or_button("search-tags")
-        expect(current_path).to eq(search_path)
+        within("#tag-search") do
+          fill_in "search", with: ("")
+          click_link_or_button("search-tags")
+          expect(current_path).to eq(profile_path)
+        end
+      end
+    end
 
-        fill_in "search", with: ("")
-        click_link_or_button("search-tags")
-        expect(current_path).to eq(profile_path)
+    it "shows users when searched" do
+      VCR.use_cassette("user_searches users test#users") do
+        visit root_path
+        login_user
+
+        click_link "Login"
+
+        expect(current_path).to eq(dashboard_feed_path)
+
+        within("#user-search") do
+          fill_in "search", with: ("mike")
+          click_link_or_button("search-users")
+          expect(current_path).to eq(user_search_path)
+        end
+
+        within("#user-search") do
+          fill_in "search", with: ("")
+          click_link_or_button("search-users")
+          expect(current_path).to eq(profile_path)
+        end
       end
     end
 
